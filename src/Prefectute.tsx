00a888055d851e44af22b67f2eeaf1f419a06391
中島秀一 <n_nakajima@nakajimashuuichinoMacBook-Pro.local>
2020-10-14 17:31:27 +0900

    PrefSRCの追加

---
+++ src/Prefectute.tsx
@@ -0,0 +1,47 @@
+import React,{Component} from 'react';
+
+
+
+class Prefecturte extends Component { 
+    render(){
+        return(
+            <div>
+                <p>北海道</p>
+                <table>
+                <tr>
+                {HokkaidoBranch.map((d)=>(<button onClick={()=>}>d.label</td>))}
+                </tr>
+                </table>
+                
+
+                
+                )
+
+                
+            </div>
+
+           
+        )
+    }
+
+    
+
+}
+
+const HokkaidoBranch=[
+    {value:"0",label:"宗谷"},
+    {value:"1",label:"上川"},
+    {value:"2",label:"オホーツク"},
+    {value:"3",label:"根室"},
+    {value:"4",label:"釧路"},
+    {value:"5",label:"十勝"},
+    {value:"6",label:"日高"},
+    {value:"7",label:"胆振"},
+    {value:"8",label:"後志"},
+    {value:"9",label:"檜山"},
+    {value:"10",label:"渡島"},
+    {value:"11",label:"石狩"},
+    {value:"12",label:"留萌"},
+];
+
+export default Prefecture 
