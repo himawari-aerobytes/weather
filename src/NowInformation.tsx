8c535f9a7ea0d47ce192dc359d707d2107b9652e
n-syuichi <syuichi.nakajima.8t@stu.hosei.ac.jp>
2020-10-24 14:12:51 +0900

    initial

--- src/NowInformation.tsx
+++ src/NowInformation.tsx
@@ -1,102 +1,96 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import WeatherIcon from './weather';
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import "./App.css";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import './App.css';
 
+type Props = {
+  Min_Temp: number | undefined;
+  Max_Temp: number | undefined;
+  precip_1h: number | undefined;
+  wind: number | undefined;
+  pref_ja: string | undefined;
+  stn_name_ja: string | undefined;
+  address: string | undefined;
+  updateAt: string | undefined;
+};
+const ta = 'a';
 
-type Props={
-    Min_Temp : number|undefined;
-    Max_Temp : number|undefined;
-    precip_1h : number|undefined;
-    wind: number | undefined;
-    pref_ja: string | undefined;
-    stn_name_ja :string | undefined;
-    address: string | undefined;
-    updateAt:string|undefined
-
-}
-const ta="a";
-
-class NowInformation extends Component<Props> { 
-    strInfo(num:number|undefined,Unit:string){
-        let information;
-        switch (num){
-            case undefined:
-                information = <p></p>;
-                break;
-            case null:
-                information = <p className="silver">情報がありません</p>
-                break;
-            default:
-                information = <p>{num+Unit}</p>;
-                break;
-        }
-        return information;
-                
+class NowInformation extends Component<Props> {
+  strInfo(num: number | undefined, Unit: string) {
+    let information;
+    switch (num) {
+      case undefined:
+        information = <p></p>;
+        break;
+      case null:
+        information = <p className="silver">情報がありません</p>;
+        break;
+      default:
+        information = <p>{num + Unit}</p>;
+        break;
     }
-  
-  makePoitStr(a: string = "", b: string = "", update: string = ""): string {
+    return information;
+  }
+
+  makePoitStr(a: string = '', b: string = '', update: string = ''): string {
     const Format = new Date(update);
     console.log(Format);
-    const year = Format.toLocaleString("ja");    
+    const year = Format.toLocaleString('ja');
     const month = Format.getMonth() + 1;
     const day = Format.getDate();
     const hour = Format.getHours();
     const minutes = Format.getMinutes();
-    const showf = year + "/" + month + "/" + day + " " + hour + ":" + minutes;
-    return a +" "+ b+" " ;
-      
+    const showf = year + '/' + month + '/' + day + ' ' + hour + ':' + minutes;
+    return a + ' ' + b + ' ';
   }
 
-    render(){
-        return (
-          <div>
-            <h2>現在の情報</h2>
-            <TableContainer component={Paper}>
-              <Table>
-                <TableRow>
-                  <TableCell></TableCell>
-                  <TableCell><p className="bluecolor">最低気温</p></TableCell>
-                  <TableCell><p className="redcolor">最高気温</p></TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>気温</TableCell>
-                  <TableCell>
-                    {this.strInfo(this.props.Min_Temp, "℃")}
-                  </TableCell>
-                  <TableCell>
-                    {this.strInfo(this.props.Max_Temp, "℃")}
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>雨量(1h)</TableCell>
-                  <TableCell colSpan={2}>
-                    <WeatherIcon rain={this.props.precip_1h} />
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>最大風速(1day)</TableCell>
-                  <TableCell colSpan={2}>
-                    {this.strInfo(this.props.wind, "m/s")}
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>観測地点</TableCell>
-                  <TableCell colSpan={2}>{this.makePoitStr(this.props.pref_ja,this.props.address,this.props.updateAt)}</TableCell>
-                </TableRow>
-              </Table>
-            </TableContainer>
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h2>現在の情報</h2>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableRow>
+              <TableCell></TableCell>
+              <TableCell>
+                <p className="bluecolor">最低気温</p>
+              </TableCell>
+              <TableCell>
+                <p className="redcolor">最高気温</p>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>気温</TableCell>
+              <TableCell>{this.strInfo(this.props.Min_Temp, '℃')}</TableCell>
+              <TableCell>{this.strInfo(this.props.Max_Temp, '℃')}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>雨量(1h)</TableCell>
+              <TableCell colSpan={2}>
+                <WeatherIcon rain={this.props.precip_1h} />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>最大風速(1day)</TableCell>
+              <TableCell colSpan={2}>{this.strInfo(this.props.wind, 'm/s')}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>観測地点</TableCell>
+              <TableCell colSpan={2}>
+                {this.makePoitStr(this.props.pref_ja, this.props.address, this.props.updateAt)}
+              </TableCell>
+            </TableRow>
+          </Table>
+        </TableContainer>
+      </div>
+    );
+  }
 }
 
-
-
-export default NowInformation
+export default NowInformation;
--- src/Point.tsx
+++ src/Point.tsx
@@ -1,35 +1,30 @@
 import React from 'react';
 
-type Props={
-    pref_ja:string;
-    stn_name_ja:string;
-    address:string;
-}
+type Props = {
+  pref_ja: string;
+  stn_name_ja: string;
+  address: string;
+};
 
-class Point extends React.Component<Props>{
-    render(){
-        return(
-            <div>
-                <table>
-                    <tr>
-                        <th align="left">県名(振興局)</th>
-                        <th align="left">観測所名</th>
-                        <th align="left">観測所住所</th>
-                    </tr>
-                    <tr>
-                        <td>{this.props.pref_ja}</td>
-                        <td>{this.props.stn_name_ja}</td>
-                        <td>{this.props.address}</td>
-                    </tr>
-                </table>
-            </div>
-        )
-    }
-
-
-
-
-
+class Point extends React.Component<Props> {
+  render() {
+    return (
+      <div>
+        <table>
+          <tr>
+            <th align="left">県名(振興局)</th>
+            <th align="left">観測所名</th>
+            <th align="left">観測所住所</th>
+          </tr>
+          <tr>
+            <td>{this.props.pref_ja}</td>
+            <td>{this.props.stn_name_ja}</td>
+            <td>{this.props.address}</td>
+          </tr>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default Point;
